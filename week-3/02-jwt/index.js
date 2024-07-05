9de54c7cb9b48a6082981111e275b10d2b6aa210
Ayush-014 <llayush1i1l@gmail.com>
2024-07-05 22:33:48 +0530

    nineth commit: completed week-3/02-jwt

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,16 +1,35 @@
 const jwt = require("jsonwebtoken");
 const jwtPassword = "secret";
+const zod = require("zod");
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string.min(6);
 
 function signJwt(username, password) {
-  
+  const usernameResponse = emailSchema.safeParse(username);
+  const passwordResponse = passwordSchema.safeParse(password);
+  if(!usernameResponse || !passwordResponse)
+      return null;
+      
+  const signature =  jwt.sign({
+      username
+  },  jwtPassword)
+  return signature;
 }
 
 function verifyJwt(token) {
- 
+  try{
+      jwt.verify(token, jwtPassword);
+      return true;
+  }catch(err){
+      console.log("jwt verification failed!")
+  }
+  return false;
 }
 
 function decodeJwt(token) {
-  
+  const decoded = jwt.decode(token);
+  return !(decoded === null);
 }
 
 module.exports = {
