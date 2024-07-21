50469c92ffe323e35106ed4d91206b492d1d5d77
Ayush-014 <llayush1i1l@gmail.com>
2024-07-21 19:05:00 +0530

    16th commit: updated testing/week-5

--- testing/week-5/backend/index.js
+++ testing/week-5/backend/index.js
@@ -1,19 +1,38 @@
 const express = require("express")
 const app = express();
+const schema = require("./types");
+const mongoose = require("mongoose");
 const port = 3000;
 
 app.use(express.json());
 
 app.post("/todo", (req,res) => {
-
+    const createPayload = req.body;
+    const parsedPayload = schema.newTodo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "Wrong inputs. Please validate them.",
+        })
+        return;
+    }
+    //put in mongoDB
 })
+
 app.get("/todos", (req,res) => {
     
 })
-app.putt("/editTodo", (req,res) => {
-    
+app.put("/todoEdit", (req,res) => {
+    const updatePayload = req.body;
+    const parsedPayload = schema.updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "Wrong inputs.Please validate inputs.",
+        })
+        return;
+    }
+    //update them
 })
 
 app.listen(port,() => {
-    console.log(`app is listening to port ${[p]}`)
+    console.log(`app is listening to port ${port}`)
 }) 
