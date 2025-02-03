008e5b2ea16a71e73076ab6566e8ac6e030fc37f
Ayush-014 <llayush1i1l@gmail.com>
2025-02-03 12:09:49 +0530

    24th commit: added week-6

---
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useCallback } from "react";
+import { useRef } from "react";
+
+// Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
+
+export function Assignment2() {
+
+  return (
+  
+  );
+}
