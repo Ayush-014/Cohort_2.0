2b703bdbaca5bef4fce0f2198a5e5dddc292411c
Ayush-014 <llayush1i1l@gmail.com>
2025-02-03 16:50:39 +0530

    26th commit: updating testing/week-6-Experiment

--- testing/week-6-Experiment/week-6-todo/src/App.jsx
+++ testing/week-6-Experiment/week-6-todo/src/App.jsx
@@ -4,32 +4,47 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count,setCount] =useState(5);
+  const [Todos, setTodos] = useState([{
+    id:1,
+    title:"task 1",
+    description:"complete POTD leetcode"
+  },
+  { id:2,
+    title:"task 2",
+    description:"complete POTD gfg"
+  },
+  { id:3,
+    title:"task 3",
+    description:"complete lec 1"
+  },
+  { id:4,
+    title:"task 4",
+    description:"complete lec 2,3"
+  } ])
+
+function addTodo() {
+  setTodos([...Todos, {
+    id: 5,
+    title: `task ${count} `,
+    description: Math.floor(Math.random()*100) + "+ pullups",
+  }]);
+  {setCount(count+1)}
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={addTodo}> Add Todo </button>
+      {Todos.map(todo => <Todo title={todo.title} description={todo.description} />)}
+    </div>
   )
+}
+
+function Todo({title,description}) {
+    return <div>
+      <h3>{title}</h3>
+      <h4>{description}</h4>
+    </div>
 }
 
 export default App
