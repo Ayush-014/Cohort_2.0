1d8eebdf94d3d5dd712d38d0863d965264952f19
Ayush-014 <llayush1i1l@gmail.com>
2025-02-08 00:19:05 +0530

    28th commit: updated testing/week-6-Experiment

--- testing/week-6-Experiment/week-6-todo/src/components/HooksPractice.jsx
+++ testing/week-6-Experiment/week-6-todo/src/components/HooksPractice.jsx
@@ -2,18 +2,38 @@ import { useState, useEffect } from 'react';
 
 function HooksPractice() {
     const [counter, setCounter] = useState(0);
+    const [title, setTitle] = useState('');
+    const [firstTitle, setfirstTitle] = useState('');
 
     useEffect(() => {
         alert(counter);
     }, [counter]);
 
+    function changetitle() {
+        setfirstTitle('my name is: '+ Math.random);
+    }
     return (
         <div>
             <button onClick={() => setCounter(prevCounter => prevCounter + 1)}>
                 Increase Count {counter}
             </button>
+            <button onClick={changetitle}>
+                Click to change title
+            </button>
+
+            <header title={firstTitle}/>
+            <header title='My name is raj'/>
+            <header title='My name is raja'/>
+            <header title='My name is rajan'/>
+            <header title='My name is rajana'/>
         </div>
     );
 }
+
+const header = memo(({title})=>{
+    return <>
+        <h3>{title}</h3>
+    </>
+})
 
 export default HooksPractice;
