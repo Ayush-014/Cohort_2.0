6eef9a6780f0e3d738c03f21cfc3b9226144f0f2
Ayush-014 <llayush1i1l@gmail.com>
2025-02-22 23:51:15 +0530

    40th commit: updated week-7 Routing

--- week-7/src/App.jsx
+++ week-7/src/App.jsx
@@ -1,35 +1,70 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const navigate = useNavigate(); {/** this hook can only be invoked inside <BrowserRouter> component */}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      {/*1. <div style={{background: "black", color:"white"}}>
+          Hi this is the topbar
+      </div> */}
+
+      {/* 2. // not actually client side routing and actually refetching the home,dashboard page. i.e. actually a hard-reloading of the page-> all the html,css,js,etc are getting fetched again.
+       <div>
+        <button onClick={() => {
+          window.location.href = "/home"; }}>
+          Home
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <button onClick={() => {
+          window.location.href = "/dashboard"; }}>
+          Dashboard
+        </button>
+      </div> */}
+
+      {/* 3. //this will not work properly bcz useNavigate expects that the component in which it is being used is inside the <BrowserRouter> component.
+      <div>
+        <button onClick={() => {
+          navigate("/home");
+        }}>
+          Home
+        </button>
+        <button onClick={() => {
+          navigate("/dashboard");
+        }}>
+          Dashboard
+        </button>
+      </div> */}
+
+      <BrowserRouter>
+        <AppBar />  {/* all the prevoius logic(just above comment i.e. 3rd one) is now wrapped inside a function to put it inside <BrowserRouter> component */}
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
+}
+
+function AppBar() {
+  const navigate = useNavigate();
+  
+  return  <div>
+  <button onClick={() => {
+    navigate("/home");
+  }}>
+    Home
+  </button>
+  <button onClick={() => {
+    navigate("/dashboard");
+  }}>
+    Dashboard
+  </button>
+</div>
 }
 
 export default App
