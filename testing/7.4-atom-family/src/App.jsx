27436a4538c941c27ca859a5c61757ae2b53c1a9
Ayush-014 <llayush1i1l@gmail.com>
2025-03-03 20:43:30 +0530

    51th commit: added testing/7.4-selector-family & 7.4-loadables, updated 7.4-atom-family

--- testing/7.4-atom-family/src/App.jsx
+++ testing/7.4-atom-family/src/App.jsx
@@ -1,25 +1,44 @@
-
+import { useEffect } from 'react';
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useSetRecoilState, useRecoilValue } from 'recoil';
 import { todosAtomFamily } from './atoms';
 
 function App() {
   return <RecoilRoot>
+    <UpdaterComponent />
     <Todo id={1}/>
+    <Todo id={2} />
+    <Todo id={2} />
     <Todo id={2} />
   </RecoilRoot>
 }
 
 function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
-
+  //  const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+   const currentTodo = useRecoilValue(todosAtomFamily(id));
   return (
     <>
-      {todo.title}
-      {todo.description}
+      {currentTodo.title}
+      {currentTodo.description}
       <br />
     </>
   )
+}
+
+function UpdaterComponent() {
+  const updateTodo = useSetRecoilState(todosAtomFamily(2));
+
+  useEffect(() => {
+    setTimeout(() => {
+      updateTodo({
+        id:2,
+        title: "new todo",
+        description: "new todo"
+      })
+    },5000)
+  }, [])
+
+  return <div></div>
 }
 
 export default App
