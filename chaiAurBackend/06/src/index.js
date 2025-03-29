b2bad8418e7bffe6de8539e973d35803a54e2847
Ayush-014 <llayush1i1l@gmail.com>
2025-03-30 00:04:19 +0530

    63th commit: updated chaiAurBackend/06: completed src/controller/user.controller.js

--- chaiAurBackend/06/src/index.js
+++ chaiAurBackend/06/src/index.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 import { app } from "./app.js"
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 // Second Approach:- 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 9000, () => {
         console.log(`@ Server is running at port : ${process.env.PORT}`);
     })
 })
