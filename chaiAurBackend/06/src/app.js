83872c2c0a57dcbef1f320fc48e9c4dcba8d1ffa
Ayush-014 <llayush1i1l@gmail.com>
2025-03-29 06:15:49 +0530

    58th commit: updated chaiAurBackend/06 utils

--- chaiAurBackend/06/src/app.js
+++ chaiAurBackend/06/src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(cors({
-    origin: process.eventNames.CORS_ORIGIN
+    origin: process.eventNames.CORS_ORIGIN,
+    credentials: true
 }))
+
+app.use(express.json({limit: "16kb"}))
 
 export { app }
