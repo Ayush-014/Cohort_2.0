3c1866962be045fb21a183b19726a2847175c30c
Ayush-014 <llayush1i1l@gmail.com>
2025-04-01 06:03:14 +0530

    67th commit: added user channel management endpoint using mongoose pipeline & added user routes

--- chaiAurBackend/06/src/controllers/user.controller.js
+++ chaiAurBackend/06/src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
 import { Subscription } from "../models/subscription.model.js"
+import mongoose from "mongoose"
 
 const generateAccessAndRefreshToken = async (userId) => {
     try{
@@ -453,6 +454,58 @@ const getUserChannnelProfile = asyncHandler( async (req,res) => {
     ))
 })
 
+const getWatchHistory = asyncHandler( async (req,res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json( new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "watch history fetched successfully"
+    ))
+})
+
 export {
     registerUser,
     loginUser,
@@ -462,5 +515,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannnelProfile
+    getUserChannnelProfile,
+    getWatchHistory
 }
