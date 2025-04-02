08ab9213d925ec776bac09b8b6cc493cb7fe042c
Ayush-014 <llayush1i1l@gmail.com>
2025-04-02 14:59:12 +0530

    68th commit: an interview problem

--- chaiAurReact/04-viteReact/src/App.jsx
+++ chaiAurReact/04-viteReact/src/App.jsx
@@ -1,10 +1,27 @@
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  const AddValue1 = () => {
+    setCounter(counter+1);
+    setCounter(counter+1);
+    setCounter(counter+1);
+    setCounter(counter+1);
+  }
+
+  const AddValue2 = () => {
+    setCounter(preCounter => preCounter+1);
+    setCounter(preCounter => preCounter+1);
+    setCounter(preCounter => preCounter+1);
+    setCounter(preCounter => preCounter+1);
+  }
 
   return (
     <>
-    hi
+        {/* an interview problem */}
+
+      <button onClick={AddValue1} />  {/* counter will be incremented by 1 at the end */}
+      <button onClick={AddValue2} />  {/* counter will be incremented by 4 at the end */}
     </>
   )
 }
