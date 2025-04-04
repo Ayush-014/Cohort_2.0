bf4e95c5981494b0fc49a53543f2fb35c1236366
Ayush-014 <llayush1i1l@gmail.com>
2025-04-04 09:22:32 +0530

    72th commit: completed chaiAurReact/07-reactRouter

--- chaiAurReact/07-reactRouter/src/components/Github/Github.jsx
+++ chaiAurReact/07-reactRouter/src/components/Github/Github.jsx
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useLoaderData } from "react-router-dom"
 
 export default function Github() {
-    const { gitId } = useParams()
-    const [data, setData] = useState([])
+    const data = useLoaderData;
+
+    // const { gitId } = useParams()
+    // const [data, setData] = useState([])
     
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${gitId}`)
-        .then((res) => res.json())
-        .then((data) => setData(data))
-        .catch((err) => console.log('Error :', err))
-    },[])
+    // useEffect(() => {
+    //     fetch(`https://api.github.com/users/${gitId}`)
+    //     .then((res) => res.json())
+    //     .then((data) => setData(data))
+    //     .catch((err) => console.log('Error :', err))
+    // },[])
 
     return (
         <div className='bg-gray-600 text-white text-center text-3xl'>
-            Github followers {data.followers}
-            <img src={data.avatar_url} alt="Github avatar" />
+            Github followers {data?.followers}
+            <img src={data?.avatar_url} alt="Github avatar" width={300}/>
         </div>
     )
+}
+
+export const githubInfoLoader = async () => {
+    const response = await fetch(`https://api.github.com/users/Ayush-014`)
+    return response.json();
 }
