19706ec549844b2f355c675e8d06245000b0933e
Ayush-014 <llayush1i1l@gmail.com>
2025-04-05 09:16:14 +0530

    74th commit: completed chaiAurReact/08-miniContext

--- chaiAurReact/08-miniContext/src/components/Profile.jsx
+++ chaiAurReact/08-miniContext/src/components/Profile.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../Context/UserContext";
 
 export default function Profile() {
-    
+    const { user } = useContext(UserContext)
+
+    if(!user)   return <div> Please Login! </div>
+
     return(
         <div>
-            Profile
+            Welcome {user.username} 
         </div>
     )
 }
