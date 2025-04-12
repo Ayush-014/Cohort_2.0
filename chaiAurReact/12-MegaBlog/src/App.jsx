ebea96e85cef21ae4b3097f4a16bc252805d9dce
Ayush-014 <llayush1i1l@gmail.com>
2025-04-12 15:02:30 +0530

    78th commit: initialized chaiAurReact/12-MegaBlog

---
+++ chaiAurReact/12-MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import config from "./config/config.js"
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  
+  return (
+    <>
+    starting mega blog project
+    </>
+  )
+}
+
+export default App
