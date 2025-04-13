d583e2f22df489a3a385f3e0e927fc94b92c3b29
Ayush-014 <llayush1i1l@gmail.com>
2025-04-13 07:02:02 +0530

    81th commit: completed redux's state management initialization

--- chaiAurReact/12-MegaBlog/src/App.jsx
+++ chaiAurReact/12-MegaBlog/src/App.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import conf from "./conf/conf.js"
-
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth.js"
+import {login, logout} from "./store/authSlice.js"
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import { Outlet } from "react-router-dom"
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
   
-  return (
-    <>
-    starting mega blog project
-    </>
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData)
+          dispatch(login({userData}))
+      else
+          dispatch(logout())
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  return loading ? null : (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+          <Header />
+          <main>
+            TODO: {/* <Outlet /> */}
+          </main>
+          <Footer />
+      </div>
+    </div>
   )
 }
 
